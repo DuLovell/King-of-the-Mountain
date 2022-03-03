866befacc9771b40bbd9cab3ab759c15ba09272a
DuLovell <bednyakov99@gmail.com>
2022-03-03 14:47:10 +0300

    Created stairsService with basic logic for placing stairs

---
+++ King of the Mountain/Assets/Scripts/Services/Environment/IStairsService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Services;
+using UnityEngine;
+
+namespace Services.Environment
+{
+	public interface IStairsService : IService
+	{
+		void PlaceStairs(Transform transformPoint);
+	}
+}
---
+++ King of the Mountain/Assets/Scripts/Services/Environment/StairsService.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Factory;
+using UnityEngine;
+
+namespace Services.Environment
+{
+	public class StairsService : IStairsService
+	{
+		private const int SIDE_MAX_STAIRS_COUNT = 10;
+		
+		private readonly IGameFactory _gameFactory;
+		
+		private readonly Vector3 _stairOffset = new Vector3(0f, 1f, 1f);
+
+		public StairsService(IGameFactory gameFactory)
+		{
+			_gameFactory = gameFactory;
+		}
+
+		public void PlaceStairs(Transform transformPoint)
+		{
+			for (int i = -SIDE_MAX_STAIRS_COUNT; i <= SIDE_MAX_STAIRS_COUNT; i++)
+			{
+				_gameFactory.CreateStair(transformPoint.position + _stairOffset * i, transformPoint.forward);
+			}
+		}
+	}
+}
