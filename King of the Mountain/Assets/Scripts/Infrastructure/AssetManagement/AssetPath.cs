884fb9186b7d7fd61ade9b0ea9c2c732d1185359
DuLovell <bednyakov99@gmail.com>
2022-03-03 14:51:09 +0300

    Created path for player and stair prefabs

--- King of the Mountain/Assets/Scripts/Infrastructure/AssetManagement/AssetPath.cs
+++ King of the Mountain/Assets/Scripts/Infrastructure/AssetManagement/AssetPath.cs
@@ -2,6 +2,8 @@ namespace Infrastructure.AssetManagement
 {
 	public static class AssetPath
 	{
+		public const string StairPath = "Prefabs/Environment/Stair";
+		public const string PlayerPath = "Prefabs/Player/Player";
 		public const string HudPath = "Prefabs/Hud/Hud";
 	}
 }
