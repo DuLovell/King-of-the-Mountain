3a7b01e30f7a4da245f196dd730588b4ad0bf8f4
DuLovell <bednyakov99@gmail.com>
2022-03-04 11:48:00 +0300

    Created ctor for GameLoopState

--- King of the Mountain/Assets/Scripts/Infrastructure/States/GameLoopState.cs
+++ King of the Mountain/Assets/Scripts/Infrastructure/States/GameLoopState.cs
@@ -1,16 +1,28 @@
-﻿namespace Infrastructure.States
+﻿using Infrastructure.Factory;
+using Services.Environment;
+using Services.Environment.Stairs;
+using UnityEngine;
+
+namespace Infrastructure.States
 {
 	public class GameLoopState : IState
 	{
-		public GameLoopState(GameStateMachine stateMachine)
+		private readonly GameStateMachine _stateMachine;
+		private readonly IStairsPlacementService _stairsPlacementService;
+		private readonly IGameFactory _gameFactory;
+
+		public GameLoopState(GameStateMachine stateMachine, IStairsPlacementService stairsPlacementService, IGameFactory gameFactory)
+		{
+			_stateMachine = stateMachine;
+			_stairsPlacementService = stairsPlacementService;
+			_gameFactory = gameFactory;
+		}
+
+		public void Enter()
 		{
 		}
 
 		public void Exit()
-		{
-		}
-
-		public void Enter()
 		{
 		}
 	}
