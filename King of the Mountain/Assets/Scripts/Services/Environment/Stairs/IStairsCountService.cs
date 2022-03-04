72443dfc68eb80cc60c770c3e22526a662ffa2c1
DuLovell <bednyakov99@gmail.com>
2022-03-04 11:43:37 +0300

    Created stairs count service

---
+++ King of the Mountain/Assets/Scripts/Services/Environment/Stairs/IStairsCountService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Services;
+using UnityEngine;
+
+namespace Services.Environment.Stairs
+{
+	public interface IStairsCountService : IService
+	{
+		int CalculateStairsDelta(Vector3 newPosition);
+	}
+}
---
+++ King of the Mountain/Assets/Scripts/Services/Environment/Stairs/StairsCountService.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Services.Environment.Stairs
+{
+	public class StairsCountService : IStairsCountService
+	{
+		private readonly Vector3 _stairOffset = new Vector3(0f, 1f, 1f);
+
+		private int _lastPlayerPositionStairNumber;
+		
+		//TODO Использовать playerStartPosition
+		private Vector3 _lastPlayerPosition = Vector3.zero;
+		
+		public int CalculateStairsDelta(Vector3 newPosition)
+		{
+			int stairsDelta = (int) (newPosition.y - _lastPlayerPosition.y) / (int) _stairOffset.y;
+			int stairNumber = _lastPlayerPositionStairNumber + stairsDelta;
+
+			_lastPlayerPositionStairNumber = stairNumber;
+			_lastPlayerPosition = newPosition;
+
+			return stairsDelta;
+		}
+	}
+}
