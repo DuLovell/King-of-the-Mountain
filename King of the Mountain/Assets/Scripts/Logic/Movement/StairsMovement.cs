17f203bb9c4d3f552405b1b7014325ada42a5f29
DuLovell <bednyakov99@gmail.com>
2022-03-04 16:18:43 +0300

    Added stairs movement

---
+++ King of the Mountain/Assets/Scripts/Logic/Movement/StairsMovement.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using Infrastructure.Services;
+using Services.Input;
+using UnityEngine;
+
+namespace Logic.Movement
+{
+	public class StairsMovement : MonoBehaviour
+	{
+		[SerializeField] private float _jumpPower = 0.5f;
+		[SerializeField] private int _jumps = 1;
+		[SerializeField] private float _jumpDuration = 1f;
+
+		private readonly Vector3 _stairOffset = new Vector3(0f, 1f, 1f);
+		
+		private IInputService _inputService;
+
+		private bool _isMoving;
+
+		private void Awake()
+		{
+			_inputService = AllServices.Container.Single<IInputService>();
+		}
+
+		private void OnEnable()
+		{
+			_inputService.OnTap += MoveVertically;
+			_inputService.OnSwipe += MoveHorizontally;
+		}
+
+		private void OnDisable()
+		{
+			_inputService.OnTap -= MoveVertically;
+			_inputService.OnSwipe -= MoveHorizontally;
+		}
+
+		private void MoveVertically()
+		{
+			StartMoving(_stairOffset);
+		}
+
+		private void MoveHorizontally(SwipeData swipeData)
+		{
+			switch (swipeData.Direction)
+			{
+				case SwipeDirection.Up:
+				case SwipeDirection.Right:
+					StartMoving(transform.right);
+					break;
+				case SwipeDirection.Down:
+				case SwipeDirection.Left:
+					StartMoving(-transform.right);
+					break;
+			}
+		}
+
+		private void StartMoving(Vector3 deltaPosition)
+		{
+			if (_isMoving) return;
+			
+			Vector3 newPosition = transform.position + deltaPosition;
+			transform.DOJump(newPosition, _jumpPower, _jumps, _jumpDuration)
+				.OnComplete(() => _isMoving = false);
+
+			_isMoving = true;
+		}
+	}
+}
