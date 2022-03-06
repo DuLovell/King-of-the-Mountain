8c8920a55b183e0ef8f0313fc6b26cdc4e8015cb
DuLovell <bednyakov99@gmail.com>
2022-03-06 12:35:01 +0300

    Player can now move on stairs

---
+++ King of the Mountain/Assets/Scripts/Logic/Movement/PlayerMover.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Data;
+using DG.Tweening;
+using Infrastructure.Services;
+using Services.Input;
+using UnityEngine;
+
+namespace Logic.Movement
+{
+	[RequireComponent(typeof(IStairsMovement))]
+	public class PlayerMover : MonoBehaviour
+	{
+		private IInputService _inputService;
+		private IStairsMovement _stairsMovement;
+		
+		private void Awake()
+		{
+			_inputService = AllServices.Container.Single<IInputService>();
+			_stairsMovement = GetComponent<IStairsMovement>();
+		}
+
+		private void OnEnable()
+		{
+			_inputService.OnTap += MoveVertically;
+			_inputService.OnSwipe += MoveHorizontally;
+		}
+
+		private void OnDisable()
+		{
+			_inputService.OnTap -= MoveVertically;
+			_inputService.OnSwipe -= MoveHorizontally;
+		}
+
+		private void MoveVertically()
+		{
+			_stairsMovement.StartMoving(_stairsMovement.StairOffset);
+		}
+
+		private void MoveHorizontally(SwipeData swipeData)
+		{
+			switch (swipeData.Direction)
+			{
+				case SwipeDirection.Up:
+				case SwipeDirection.Right:
+					_stairsMovement.StartMoving(transform.right);
+					break;
+				case SwipeDirection.Down:
+				case SwipeDirection.Left:
+					_stairsMovement.StartMoving(-transform.right);
+					break;
+			}
+		}
+	}
+}
