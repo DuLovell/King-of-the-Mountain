9caae01fc8a39914131e0b5965059763c9f4becf
DuLovell <bednyakov99@gmail.com>
2022-03-06 12:34:41 +0300

    Implemented stairs movement

---
+++ King of the Mountain/Assets/Scripts/Logic/Movement/IStairsMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Logic.Movement
+{
+	public interface IStairsMovement
+	{
+		Vector3 StairOffset { get; }
+		void StartMoving(Vector3 deltaPosition);
+	}
+}
--- King of the Mountain/Assets/Scripts/Logic/Movement/StairsMovement.cs
+++ King of the Mountain/Assets/Scripts/Logic/Movement/StairsMovement.cs
@@ -1,71 +1,23 @@
-﻿using System;
-using System.Collections;
-using Data;
+﻿using Data;
 using DG.Tweening;
-using Infrastructure.Services;
-using Services.Input;
 using UnityEngine;
 
 namespace Logic.Movement
 {
-	public class StairsMovement : MonoBehaviour
+	public class StairsMovement : MonoBehaviour, IStairsMovement
 	{
 		[SerializeField] private float _jumpPower = 0.5f;
 		[SerializeField] private int _jumps = 1;
 		[SerializeField] private float _jumpDuration = 1f;
 
-		private readonly Vector3 _stairOffset = Config.StairOffset;
+		private Vector3 _newPosition = Config.PlayerStartPosition;
 		
-		private IInputService _inputService;
+		public Vector3 StairOffset => Config.StairOffset;
 
-		private bool _isMoving;
-
-		private void Awake()
+		public void StartMoving(Vector3 deltaPosition)
 		{
-			_inputService = AllServices.Container.Single<IInputService>();
-		}
-
-		private void OnEnable()
-		{
-			_inputService.OnTap += MoveVertically;
-			_inputService.OnSwipe += MoveHorizontally;
-		}
-
-		private void OnDisable()
-		{
-			_inputService.OnTap -= MoveVertically;
-			_inputService.OnSwipe -= MoveHorizontally;
-		}
-
-		private void MoveVertically()
-		{
-			StartMoving(_stairOffset);
-		}
-
-		private void MoveHorizontally(SwipeData swipeData)
-		{
-			switch (swipeData.Direction)
-			{
-				case SwipeDirection.Up:
-				case SwipeDirection.Right:
-					StartMoving(transform.right);
-					break;
-				case SwipeDirection.Down:
-				case SwipeDirection.Left:
-					StartMoving(-transform.right);
-					break;
-			}
-		}
-
-		private void StartMoving(Vector3 deltaPosition)
-		{
-			if (_isMoving) return;
-			
-			Vector3 newPosition = transform.position + deltaPosition;
-			transform.DOJump(newPosition, _jumpPower, _jumps, _jumpDuration)
-				.OnComplete(() => _isMoving = false);
-
-			_isMoving = true;
+			_newPosition += deltaPosition;
+			transform.DOJump(_newPosition, _jumpPower, _jumps, _jumpDuration);
 		}
 	}
 }
